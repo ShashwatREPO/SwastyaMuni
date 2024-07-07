48c7407e4ee894947b06ae61505562e67b0ea62b
PervyArtorias <22bcs10035@gmail.com>
2024-07-07 12:13:51 +0530

    tailwind configs

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgroundGreen: "#F8FBF7",
+        accentGreen: "#508D4E",
+        DarkGreen: "#1A5319",
+      },
+      fontFamily: {
+        poppins: "Poppins",
+      },
+    },
   },
   plugins: [],
-}
+};
