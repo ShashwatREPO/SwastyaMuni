563b287507036e63ac29bbbf83b1e88d13c8313d
Kartik Aslia <s7011068163@gmail.com>
2024-07-07 11:19:18 +0530

    conflict resolved

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const authRoute = require('./Routes/authRoutes');
+const User = require('./Database/userSchema');
+const bcrypt = require('bcryptjs');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
-    res.send('Hello World');
-}
-);
+  if (req.isAuthenticated()) {
+    res.send('hello world');
+  } else {
+    res.send('first authenticate');
+  }
+});
 
