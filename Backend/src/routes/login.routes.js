fd5364f1e878d04049dd9b03cb0b5963ae4aa5a9
PervyArtorias <22bcs10035@gmail.com>
2024-07-11 15:51:39 +0530

    fixed synatx error

--- Backend/src/routes/login.routes.js
+++ Backend/src/routes/login.routes.js
@@ -1,30 +1,32 @@
 import passportLocal from "../middlewares/passportLocalConfig.middleware.js";
-import express from "express"; 
+import express from "express";
 import jwt from "jsonwebtoken";
 
+const router = express.Router();
 
-const router = express.Router() ; 
-
-router.post('/', async (req, res, next) => {
-    passportLocal.authenticate('local', { session: false }, (err, user, info) => {
-        if (err) {
-            return next(err);
-        }
-        if (!user) {
-            return res.status(401).json({ 
-                valid : false, 
-                message: 'Unauthorized' 
-            });
-        req.login(user, { session: false }, async (error) => {
-            if (error) return next(error);
-            const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET || 'your_jwt_secret_here');
-            return res.json({
-                valid : true , 
-                token : token 
-            });
-        });
-    })(req, res, next);
+router.post("/", async (req, res, next) => {
+  passportLocal.authenticate("local", { session: false }, (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.status(401).json({
+        valid: false,
+        message: "Unauthorized",
+      });
+    }
+    req.login(user, { session: false }, async (error) => {
+      if (error) return next(error);
+      const token = jwt.sign(
+        { id: user.id },
+        process.env.JWT_SECRET || "your_jwt_secret_here"
+      );
+      return res.json({
+        valid: true,
+        token: token,
+      });
+    });
+  })(req, res, next);
 });
 
-
-export default router ; 
+export default router;
