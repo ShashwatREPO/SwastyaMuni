ee78f040c54106c8f341bb283ef3759da068ee21
PervyArtorias <22bcs10035@gmail.com>
2024-07-12 18:40:35 +0530

    changes in backed

--- Backend/src/routes/googleAuth.routes.js
+++ Backend/src/routes/googleAuth.routes.js
@@ -1,18 +1,29 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import passportGoogle from "../middlewares/passportGooglAuthConfig.middleware.js"; 
+import passportGoogle from "../middlewares/passportGooglAuthConfig.middleware.js";
 
+const router = express.Router();
 
-const router = express.Router(); 
+router.get(
+  "/",
+  passportGoogle.authenticate("google", { scope: ["profile", "email"] })
+);
 
-router.get('/', passportGoogle.authenticate('google', { scope: ['profile', 'email'] }));
-
-router.get('/callback', passportGoogle.authenticate('google', { session: false, failureRedirect: '/login' }), (req, res) => {
-    const token = jwt.sign({ id: req.user.id }, process.env.JWT_SECRET || 'your_jwt_secret_here');
+router.get(
+  "/callback",
+  passportGoogle.authenticate("google", {
+    session: false,
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    const token = jwt.sign(
+      { id: req.user.id },
+      process.env.JWT_SECRET || "your_jwt_secret_here"
+    );
     res.json({
-        valid : true, 
-        token : token
-    }); 
-});
+      token: token,
+    });
+  }
+);
 
-export default router ;
+export default router;
--- Backend/src/routes/signup.routes.js
+++ Backend/src/routes/signup.routes.js
@@ -1,51 +1,59 @@
-import express from "express"; 
+import express from "express";
 import { User } from "../models/users.models.js";
 import { UserData } from "../models/userData.models.js";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt"; 
+import bcrypt from "bcrypt";
 
-const router = express.Router(); 
+const router = express.Router();
 
 router.post("/", async (req, res, next) => {
-    const { fullName, email, password, age, allergies, pastDisease, currentDisease } = req.body;
-    let newUser ; 
-    try {
-        let existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ 
-                valid : false, 
-                message: "User already exists with this email" 
-            });
-        }
-
-        const newUserData = new UserData({ age : parseInt(age), allergies, pastDisease, currentDisease });
-        await newUserData.save();
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        newUser = new User({
-            fullName,
-            email,
-            password : hashedPassword,
-            userData: newUserData._id
-        });
-        await newUser.save();
-
-       const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET || "your_jwt_secret_here");
-        res.status(201).json({
-            valid : true,  
-            token : token 
-        });
-    } catch (error) {
-        console.error("Signup failed:", error);
-
-        if (error.name === "ValidationError" && error.errors && error.errors.email && error.errors.email.kind === "unique") {
-            await UserData.deleteOne({ _id: newUser._id });
-        }
-        
-        res.status(500).json({ message: "Signup failed" });
+  const { fullName, email, password } = req.body;
+  let newUser;
+  try {
+    let existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        valid: false,
+        message: "User already exists with this email",
+      });
     }
+
+    if (!password) return res.status(501).json({ message: "pass not found" });
+
+    // const newUserData = new UserData({ age : parseInt(age), allergies, pastDisease, currentDisease });
+    // await newUserData.save();
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+
+    const token = jwt.sign(
+      { id: newUser.id },
+      process.env.JWT_SECRET || "your_jwt_secret_here"
+    );
+    res.status(201).json({
+      valid: true,
+      token: token,
+    });
+  } catch (error) {
+    console.error("Signup failed:", error);
+
+    if (
+      error.name === "ValidationError" &&
+      error.errors &&
+      error.errors.email &&
+      error.errors.email.kind === "unique"
+    ) {
+      await UserData.deleteOne({ _id: newUser._id });
+    }
+
+    res.status(500).json({ message: "Signup failed" });
+  }
 });
 
-
-export default router ; 
+export default router;
