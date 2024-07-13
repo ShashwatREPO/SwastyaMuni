54400c639eac42225eb7ee26df0ae76b7d9f7652
PervyArtorias <22bcs10035@gmail.com>
2024-07-14 02:51:43 +0530

    made changes to google auth route to redirect the token through url

--- Backend/src/routes/googleAuth.routes.js
+++ Backend/src/routes/googleAuth.routes.js
@@ -13,16 +13,14 @@ router.get(
   "/callback",
   passportGoogle.authenticate("google", {
     session: false,
-    failureRedirect: "/login",
+    failureRedirect: "http://localhost:5173/login",
   }),
   (req, res) => {
     const token = jwt.sign(
       { id: req.user.id },
       process.env.JWT_SECRET || "your_jwt_secret_here"
     );
-    res.json({
-      token: token,
-    });
+    res.redirect(`http://localhost:5173/login?token=${token}`);
   }
 );
 
